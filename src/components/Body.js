9824693ae099421c81105073e0d276590ca35cb3
Sarthak <sarth99106@gmail.com>
2024-06-01 01:19:05 -0400

    Sorting added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,21 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurant from "../../utils/mockData";
-
-
+import React, { useState } from 'react';
 
 const Body = () => {
+
+  const [resList, setResList] = useState(restaurant);
+  console.log(restaurant);
+  console.log(resList);
   return (
     <div className="body">
       <div className="search-container">
         <input type="text" placeholder="Enter Restaurant"></input>
         <button>Submit</button>
+      </div>
+
+      <div className="sort-btn">
+        <button onClick = {() => setResList(() => resList.filter(rest => rest.data.avgRating > 4))}>Sort By 4 ⭐</button>
       </div>
 
       <div className="res-container">
@@ -21,7 +28,7 @@ const Body = () => {
                     return restaurantCards;
                 })()
         } */}
-        {restaurant.map((restData, index) => (
+        {resList.map((restData, index) => (
           <RestaurantCard key={restData.data.id} restData={restData} />
         ))}
       </div>
