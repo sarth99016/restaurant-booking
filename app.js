1734578fee4de294ec9047915a2279dc5dfea631
Sarthak <sarth99106@gmail.com>
2024-06-03 03:32:34 -0400

    Updated Files

--- app.js
+++
@@ -1,3 +0,0 @@
-const heading = React.createElement("div", {id:"parent"}, [React.createElement("div", {id:"child"}, React.createElement("h1", {id:"heading"}, "Hello World")), React.createElement("div", {id:"child"}, React.createElement("h1", {id:"heading"}, "Hello World"))]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
